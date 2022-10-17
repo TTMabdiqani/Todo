6668859f07c28259a9d96a8dd3ae90731ed4c179
MMTabdiqani <abdiqani.omar1@gmail.com>
2022-10-17 17:15:09 +0300

    added add item and marasdone fuctionality

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function addtodo(){
     const inputelement = document.querySelector('input');
     const value = inputelement.value;
+    if(value == ""){
+
+        return;
+    }
+   
+
 
     const todoContainer = document.createElement('div');
     todoContainer.className = "flex justify-between items-center";
@@ -9,7 +15,8 @@ function addtodo(){
 
     const todoButton = document.createElement('button');
     todoButton.textContent = 'Done';
-    todoButton.className = "bg-green-500 p-2 rounded-lg";
+    todoButton.className = "bg-green-500 p-2 rounded-lg btn";
+    todoButton.onclick = markAsDone;
 
     todoContainer.appendChild(todoTitle);
     todoContainer.appendChild(todoButton);
@@ -19,7 +26,20 @@ function addtodo(){
     todesconterner.appendChild(todoContainer);
     
    inputelement.value = "";
+   
 
 
 
+
+}
+
+
+function markAsDone(event){
+ const clickedButton = event.target;
+ clickedButton.classList.add('hidden');
+ 
+
+const todoItem = clickedButton.previousElementSibling;
+todoItem.classList.add('line-through');
+console.log(todoItem);
 }
